e10b436bd2a73b02116f42df54685141c52aa903
Farrel Jordan <farreljordan80@gmail.com>
2024-05-27 12:25:46 +0700

    handle error

--- tests/moq.spec.ts
+++ tests/moq.spec.ts
@@ -4,39 +4,39 @@ const resultPath = './downloads'
 const downloadTimeout = 120000;
 
 test.beforeEach(async ({ page }) => {
-  test.setTimeout(downloadTimeout);
-  try {
-    await page.goto('https://tugas-akhir.farreljordan.com/');
-    await page.getByRole('button', { name: 'Start' }).click();
-    await page.getByText('click to play').click();
-  } catch (e) {
-    console.log("Error:", e)
-    await page.goto('https://tugas-akhir.farreljordan.com/');
-    await page.getByRole('button', { name: 'Start' }).click();
-    await page.getByText('click to play').click();
+  while (true) {
+    try {
+      await page.goto('https://tugas-akhir.farreljordan.com/');
+      await page.getByRole('button', { name: 'Start' }).click();
+      await page.getByText('click to play').click();
+
+      await expect(page.locator('label.audio.label span')).not.toHaveText('');
+      await expect(page.locator('label.video.label span')).not.toHaveText('');
+      await expect(page.getByText('click to play')).toBeHidden()
+
+      break;
+    } catch (error) {
+      console.log('Playing the video failed, retrying...');
+    }
   }
+  test.setTimeout(downloadTimeout);
 });
 
 test('test stream', async ({ page }) => {
-  await expect(page.getByText('click to play')).toBeHidden()
-  // TODO: expect audio label and video label not empty
   await downloadResults(page, '/stream/')
 });
 
 test('test datagram', async ({ page }) => {
-  await expect(page.getByText('click to play')).toBeHidden()
   await page.locator("#category").selectOption({value: "1"})
   await downloadResults(page, '/datagram/')
 });
 
 test('test hybrid', async ({ page }) => {
-  await expect(page.getByText('click to play')).toBeHidden()
   await page.locator("#category").selectOption({value: "2"})
   await downloadResults(page, '/hybrid/')
 });
 
 test('test auto', async ({ page }) => {
-  await expect(page.getByText('click to play')).toBeHidden()
   await page.locator("#category").selectOption({value: "3"})
   await downloadResults(page, '/auto/')
 });
